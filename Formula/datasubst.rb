5e690747e73bb4258c2f10bb3c0d64c8bcf836ed
GitHub Action <action@github.com>
2021-03-20 18:35:35 +0000

    Update datasubst formula to version 0.2.1

--- Formula/datasubst.rb
+++ Formula/datasubst.rb
@@ -5,23 +5,23 @@ class Datasubst < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.2.1/datasubst-v0.2.1-darwin-amd64.tar.gz'
-    sha256 'a0c68f7ef8875d9be62b545bdc1478a51df5922c3fac25db440cd230342a0331'
+    sha256 'a9de8e8f9d8294a90140858dbbb33f86e57f1de72d93e1dca388e6567eae3217'
   end
   if OS.mac? && Hardware::CPU.arm?
     url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.2.1/datasubst-v0.2.1-darwin-arm64.tar.gz'
-    sha256 '95309fdf74fcc1dc6b5cd42b083722c4c227f66731737eeb94b9f1b14731979a'
+    sha256 '0f84affd958831175b2dc09b9f613029dce91fcf9bad74a70ccaebb15f77a198'
   end
   if OS.linux? && Hardware::CPU.intel?
     url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.2.1/datasubst-v0.2.1-linux-amd64.tar.gz'
-    sha256 'e0dbf313e683bb3ffdeb524babd1dc0aa94b29a5cd7bc61f590c805e93f4d67a'
+    sha256 '167fdb74c42f7579377c54a413c1a0a37119c5092af109ae873759c6987ec8d8'
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.2.1/datasubst-v0.2.1-linux-arm.tar.gz'
-    sha256 'af9865fb008a9c0f0dc20dc24b7d9b15fec33795de8b90bb8933f93ebae4beda'
+    sha256 'b389434ea0c0941fb700f69b20b62310f6aaca37fd8eafefbbec4d141fa64a21'
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.2.1/datasubst-v0.2.1-linux-arm64.tar.gz'
-    sha256 '92ab13f5fc4bb452e9b13bb1d6e7fe6704714b07febc679b15fe27de2c4dc27e'
+    sha256 ''
   end
 
   def install
