137161a1e2e74597f9523ca6debddc16458b6ce1
GitHub Action <action@github.com>
2021-03-28 19:37:50 +0000

    Update datasubst formula to version 0.3.0

--- Formula/datasubst.rb
+++ Formula/datasubst.rb
@@ -1,27 +1,27 @@
 class Datasubst < Formula
   desc 'A simple go template based tool based that uses structured data such as JSON and YAML as data sources.'
   homepage 'https://github.com/marcelocarlos/datasubst'
-  version '0.2.1'
+  version '0.3.0'
 
   if OS.mac? && Hardware::CPU.intel?
-    url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.2.1/datasubst-v0.2.1-darwin-amd64.tar.gz'
-    sha256 '8108f2ad47b7681245af6a88184932caf751b5fa6ccea4bb7dd224fe21041671'
+    url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.3.0/datasubst-v0.3.0-darwin-amd64.tar.gz'
+    sha256 '6651221b696df444cf3cf67ba5153a80faf8ca0d4d28a6e8696cfbc2123edd88'
   end
   if OS.mac? && Hardware::CPU.arm?
-    url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.2.1/datasubst-v0.2.1-darwin-arm64.tar.gz'
-    sha256 'e1125d644978a0b5f871da0fef3b5655b06d0f0e7dd38c7695272d6ade57e1b3'
+    url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.3.0/datasubst-v0.3.0-darwin-arm64.tar.gz'
+    sha256 '72c7a128555fb5dbc07bc60e63b96988f6396456d3fd2ac09f8e9eddbd618b04'
   end
   if OS.linux? && Hardware::CPU.intel?
-    url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.2.1/datasubst-v0.2.1-linux-amd64.tar.gz'
-    sha256 '6002004e3c17ca092034c9b7de650b8d396e4a5e5e8f15d4cb8feff61854a31e'
+    url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.3.0/datasubst-v0.3.0-linux-amd64.tar.gz'
+    sha256 '56fada7032203f2bfe2a26bf1b19e22f1035d90d4356be4f5567b659762b9945'
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.2.1/datasubst-v0.2.1-linux-arm.tar.gz'
-    sha256 '338e6f7aff6277b6e85cd19be6a31b02fc633ec35b1915aaea282f78125ccd6c'
+    url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.3.0/datasubst-v0.3.0-linux-arm.tar.gz'
+    sha256 '68522925c1f96443589e104c105a19b7b8462f8306f4ab49216bcb74ea24e467'
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.2.1/datasubst-v0.2.1-linux-arm64.tar.gz'
-    sha256 '50772906f26bee181ef72d595d224a1a2c6117d881e78d4cd3915672e85e13bb'
+    url 'https://github.com/marcelocarlos/datasubst/releases/download/v0.3.0/datasubst-v0.3.0-linux-arm64.tar.gz'
+    sha256 '08074e7727b19c5834a8f9d477af13447c87d6230816075c29b69fde1f77eea8'
   end
 
   def install
